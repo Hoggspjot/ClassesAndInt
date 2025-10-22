bdfd704568ce409c5c9c23e6e7c6c10c9461f359
Hoggspjot <vingisaar@gmail.com>
2025-10-22 23:39:36 +0300

    nested classes

---
+++ src/main/java/day221025/task1/Main.java
@@ -0,0 +1,22 @@
+package day221025.task1;
+
+public class Main {
+    public static void main(String[] args) {
+        // Создаём экземпляр статического вложенного класса
+        // Важно: объект внешнего класса Spellbook не нужен
+        Spellbook.BasicCharm charm = new Spellbook.BasicCharm();
+
+        // Активируем заклинание
+        charm.castSpell();
+    }
+}
+
+class Spellbook {
+
+    static class BasicCharm {
+
+        public void castSpell() {
+            System.out.println("Hello from static basic charm!");
+        }
+    }
+}
