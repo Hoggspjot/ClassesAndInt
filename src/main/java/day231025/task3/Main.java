6cbfa746406c5810e41d6a68d79acc48c85a3b88
Hoggspjot <vingisaar@gmail.com>
2025-10-23 23:16:51 +0300

    Анонимные классы

---
+++ src/main/java/day231025/task3/Main.java
@@ -0,0 +1,27 @@
+package day231025.task3;
+
+// Главный класс с методом main
+public class Main {
+    public static void main(String[] args) {
+        // Создаем объект лаборатории и запускаем механизм раскрытия секрета
+        SecureLab lab = new SecureLab();
+        lab.revealSecret();
+    }
+}
+
+// Класс секретной лаборатории
+class SecureLab {
+    // Приватное поле с секретом
+    private String classifiedSecret = "Секрет раскрыт!";
+
+    // Метод, который "раскрывает" секрет через анонимный класс
+    void revealSecret() {
+        // Создаем объект интерфейса Runnable через анонимный класс
+
+        Runnable revealMechanism = () -> {
+            System.out.println(classifiedSecret);
+        };
+        // Вызываем метод run() прямо здесь, без создания отдельного потока
+        revealMechanism.run();
+    }
+}
