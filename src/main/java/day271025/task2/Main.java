141eeb637f138aa6891e054bd5130327f083f560
Hoggspjot <vingisaar@gmail.com>
2025-10-27 22:35:06 +0300

    Полиморфизм и наследование

---
+++ src/main/java/day271025/task2/Main.java
@@ -0,0 +1,31 @@
+package day271025.task2;
+
+public class Main {
+    public static void main(String[] args) {
+        // Полиморфизм: ссылка типа Animal указывает на объект фактического типа Cat
+        Animal mysteriousCreature = new Cat();
+
+        // Вызов sleep() приведёт к вызову Cat.makeSound() благодаря динамическому связыванию
+        mysteriousCreature.sleep();
+    }
+}
+
+class Animal {
+
+    void makeSound() {
+        System.out.println("Some sound");
+    }
+
+    void sleep() {
+        System.out.println("Animal is going to sleep...");
+        this.makeSound();
+    }
+}
+
+class Cat extends Animal {
+
+    @Override
+    void makeSound() {
+        System.out.println("Meow!");
+    }
+}
