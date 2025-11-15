2721f7b7353f354a161c2e588493d71d7c2d719b
Hoggspjot <vingisaar@gmail.com>
2025-11-15 22:02:48 +0300

    Разминка 16 первый вариант

---
+++ src/main/java/day151125/task1/Main.java
@@ -0,0 +1,49 @@
+package day151125.task1;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<List<Integer>> data = readInput();
+        int result;
+
+        result = findMinNumber(data);
+
+        System.out.println(result);
+    }
+
+    public static int findMinNumber(List<List<Integer>> data) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < data.size(); i++) {
+            for (int j = 0; j < data.get(i).size(); j++) {
+                if (data.get(i).get(j) < min) {
+                    min = data.get(i).get(j);
+                }
+            }
+        }
+        return min;
+    }
+
+    public static List<List<Integer>> readInput() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        Gson gson = new Gson();
+        Type listType = new TypeToken<List<List<Integer>>>(){}.getType();
+        return gson.fromJson(input, listType);
+    }
+}
+
+
+/*У вас есть переменная data, которая содержит входные пользовательские данные.
+
+data - двумерный список из элементов целых чисел.
+
+Напишите код, который находит минимальное число в двумерном списке произвольного размера и записывает результат в переменную result.*/
+
+
+/*    */
