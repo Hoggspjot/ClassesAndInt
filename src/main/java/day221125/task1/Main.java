c76f9a285fbd7c96336f180d921e8a2ef9189912
Hoggspjot <vingisaar@gmail.com>
2025-11-22 22:26:58 +0300

    Разминка 23

---
+++ src/main/java/day221125/task1/Main.java
@@ -0,0 +1,73 @@
+package day221125.task1;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Pair<Integer, List<String>> input = readInput();
+        int power = input.getFirst();
+        List<String> items = input.getSecond();
+        int result = increasePower(power, items);
+        System.out.println(result);
+    }
+
+    public static int increasePower(int power, List<String> items) {
+        for (String s : items) {
+            if (s.equals("Энергетик")) {
+                power+=5;
+            }
+            if (s.equals("Кофе")) {
+                power += 10;
+            }
+        }
+        return power < 100 ? power : 100;
+    }
+
+    public static Pair<Integer, List<String>> readInput() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        Gson gson = new Gson();
+        Type listType = new TypeToken<List<String>>(){}.getType();
+        List<String> items = gson.fromJson(input.split(" \\| ")[1], listType);
+        int power = Integer.parseInt(input.split(" \\| ")[0]);
+
+        return new Pair<>(power, items);
+    }
+}
+
+class Pair<F, S> {
+    private final F first;
+    private final S second;
+
+    public Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public F getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+}
+
+/*У вас есть переменные power и items, которые содержат входные пользовательские данные.
+
+items - список из элементов строк.
+
+Напишите код, который увеличивает значение переменной power в зависимости от того, сколько улучшений/бустов находится в списке items и записывается результат в переменную result.
+
+Улучшения:
+
+"Энергетик": power + 5
+"Кофе": power + 10
+Важно!
+
+Итоговое значение переменной result не должно быть больше 100. Если значение переменной result получается больше 100, тогда устанавливаем значении переменной result равной 100.*/
