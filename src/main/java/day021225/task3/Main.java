7928cfc615148e90b6430865eaf5f18a7d8b4975
Hoggspjot <vingisaar@gmail.com>
2025-12-02 22:00:14 +0300

    Разминка 37 классы

---
+++ src/main/java/day021225/task3/Main.java
@@ -0,0 +1,46 @@
+package day021225.task3;
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Employee employee = new Employee();
+        employee.surname = scanner.next();
+        employee.sex = scanner.next();
+        employee.age = scanner.nextInt();
+        employee.salary = scanner.nextDouble();
+
+        if (employee.isPensioner()) {
+            employee.changeSalary(0.9);
+        }
+        employee.show();
+    }
+}
+
+class Employee {
+    String surname;
+    String sex;
+    int age;
+    double salary;
+
+    public boolean isPensioner() {
+        if (sex.equals("male") && age >= 63) {
+            return true;
+        } else if (sex.equals("female") && age >= 58) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void changeSalary(double koef) {
+        salary = salary * koef;
+    }
+
+    public void show() {
+        System.out.printf("%s;%s;%d;%.0f",surname,sex,age,salary);
+    }
+
+
+}
