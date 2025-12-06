509c1241784aa742d39f4268566613120fea0282
Hoggspjot <vingisaar@gmail.com>
2025-12-06 23:27:13 +0300

    Разминка стрим

---
+++ src/main/java/day061225/task1/Main.java
@@ -0,0 +1,34 @@
+package day061225.task1;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<List<Integer>> data = readInput();
+        List<Integer> result = rowSum(data);
+        System.out.println(new Gson().toJson(result));
+    }
+
+    public static List<Integer> rowSum(List<List<Integer>> data) {
+        return data.stream().map(x -> x.stream().mapToInt(Integer::intValue).sum()).collect(Collectors.toList());
+    }
+
+    public static List<List<Integer>> readInput() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        return new Gson().fromJson(input, new TypeToken<List<List<Integer>>>() {}.getType());
+    }
+}
+
+
+/**
+ * У вас есть переменная data которая содержит входные пользовательские данные.
+ *
+ * data - двумерный массив из элементов целых чисел.
+ *
+ * Напишите код, который находит сумму элементов для каждой строки двумерного массива data и записывает результат в новый массив.
+ *
+ * Новый массив необходимо записать в переменную result.*/
