8f8710512d2561da9360be3f957531e35b10779f
Hoggspjot <vingisaar@gmail.com>
2025-12-09 19:01:45 +0300

    разминка 45 стримы

--- src/main/java/december/day091225/Main.java
+++ src/main/java/december/day091225/Main.java
@@ -3,6 +3,7 @@ package december.day091225;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,15 +13,9 @@ public class Main {
     }
 
     public static int calculateDifference(List<Integer> prices) {
-        int sum = -1 * prices.get(0);
-        for (int i = 1; i < prices.size(); i++) {
-            if (i % 2 != 0) {
-                sum += prices.get(i);
-            } else {
-                sum -= prices.get(i);
-            }
-        }
-        return sum;
+
+        return IntStream.range(0, prices.size())
+                .map(i -> prices.get(i) % 2 != 0 ? prices.get(i) : -prices.get(i)).sum();
     }
 
     public static List<Integer> readInput() {
