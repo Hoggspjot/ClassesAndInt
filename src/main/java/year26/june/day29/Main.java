e4688b0665746f32d4801caa4c2f1e4a1e0fcb84
Hoggspjot <vingisaar@gmail.com>
2026-06-29 23:37:17 +0300

    Заново 29.06

---
+++ src/main/java/year26/june/day29/Main.java
@@ -0,0 +1,27 @@
+package year26.june.day29;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(i+" ");
+        }
+
+        System.out.println();
+
+        int i = 0;
+        while (i < 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+
+        System.out.println();
+
+        i = 0;
+        do {
+            System.out.print(i+" ");
+            i++;
+        } while (i < 10);
+
+    }
+}
