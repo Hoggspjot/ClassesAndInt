4f2df2a5a64cc64b3ce3217a43e7d539bcba7260
Hoggspjot <vingisaar@gmail.com>
2026-07-02 23:22:19 +0300

    Заново 02.07

---
+++ src/main/java/year2026/july/day02/Main.java
@@ -0,0 +1,17 @@
+package year2026.july.day02;
+
+public class Main {
+    public static void main(String[] args){
+
+        int var1;
+        int var2;
+        double var3, var4;
+        var1 = 10;
+        var2 = var1 / 2;
+        var3 = 10.0;
+        System.out.println(var1 + " " + var2);
+
+        System.out.println(var1 / 4);
+        System.out.println(var3 / 4);
+    }
+}
---
+++ src/main/java/year26/june/day30/Main.java
@@ -0,0 +1,23 @@
+package year26.june.day30;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] array = {4, 3, 2, 1, 4, 6, 7, 8, 0};
+
+
+    }
+
+
+    public static void bubbleSort(int[] arr) {
+
+        int temp = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+
+            }
+        }
+
+    }
+}
