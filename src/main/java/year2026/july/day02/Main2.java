c248be2b19d264b79fb8d160f030d574c0a8a1d7
Hoggspjot <vingisaar@gmail.com>
2026-07-02 23:25:45 +0300

    Заново 02.07

---
+++ src/main/java/year2026/july/day02/Main2.java
@@ -0,0 +1,13 @@
+package year2026.july.day02;
+
+public class Main2 {
+    public static void main(String[] args) {
+
+        double gallons;
+        double liters;
+
+        gallons = 5;
+        liters = gallons * 3.7854;
+        System.out.println(gallons+" галлонов соответствует "+liters+" литрам");
+    }
+}
