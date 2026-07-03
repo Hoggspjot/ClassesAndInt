8547af7d62013d0b821e1bd36662ad546a32b3c5
Hoggspjot <vingisaar@gmail.com>
2026-07-03 17:39:05 +0300

    03.07 заново

--- src/main/java/year2026/july/day03/Main.java
+++ src/main/java/year2026/july/day03/Main.java
@@ -1,10 +1,28 @@
 package year2026.july.day03;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int num = 0;
+        System.out.println("Введи число");
+        try (Scanner scanner = new Scanner((System.in))) {
+
+            num = scanner.nextInt();
+        }
 
 
-
-
+        if (num < 0) {
+            System.out.println(false);
+        }
+        if (num >= 1 && num < 2) {
+            System.out.println(true);
+        }
+        if (num == 0) {
+            System.out.println("Great!");
+        }
     }
 }
