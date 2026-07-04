fef3ecc4c9135c82e56a7c6de200761fdaa6047e
Hoggspjot <vingisaar@gmail.com>
2026-07-04 23:36:39 +0300

    Заново 04.07

---
+++ src/main/java/year2026/july/day04/Main.java
@@ -0,0 +1,24 @@
+package year2026.july.day04;
+
+public class Main {
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 2;
+        b = 3;
+
+        if (a > 0) {
+            System.out.println("first a is Great!");
+        }
+        if (b < 0) {
+            System.out.println("first b Great!");
+        }
+        c = a - b;
+        if (c > 0) {
+            System.out.println(" a is great!");
+        }
+        c = b - a;
+        if (c > 0) {
+            System.out.println("b is great!");
+        }
+    }
+}
