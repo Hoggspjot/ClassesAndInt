f494bb55eb184ced1b2559c3b0fb18fc3f6b57ff
Hoggspjot <vingisaar@gmail.com>
2026-09-01 23:59:58 +0300

    Заново 01/09

---
+++ src/main/java/year2026/september/day01/Main.java
@@ -0,0 +1,30 @@
+package year2026.september.day01;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> list = Arrays.asList("apple", "banana", "apple", "cherry", "banana", "apple");
+
+    }
+
+    public static HashMap<String, Integer> counter(List<String> list) {
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String str : list) {
+
+
+
+        }
+
+
+        return
+    }
+
+}
