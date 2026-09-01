fd5290a6138f4d53b5ef3f5b72f44b417be0e0ca
Hoggspjot <vingisaar@gmail.com>
2026-09-02 00:08:12 +0300

    Заново 01/09

--- src/main/java/year2026/september/day01/Main.java
+++ src/main/java/year2026/september/day01/Main.java
@@ -3,28 +3,35 @@ package year2026.september.day01;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
 
         List<String> list = Arrays.asList("apple", "banana", "apple", "cherry", "banana", "apple");
 
+        Map<String, Integer> map =  counter(list);
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " - "+ entry.getValue());
+        }
     }
 
-    public static HashMap<String, Integer> counter(List<String> list) {
+    public static Map<String, Integer> counter(List<String> list) {
 
         HashMap<String, Integer> map = new HashMap<>();
 
-        for (String str : list) {
-
-
-
+        if (list.size() == 0) {
+            return map;
         }
 
-
-        return
+        for (String str : list) {
+            if (!map.containsKey(str)) {
+                map.put(str, 1);
+            } else {
+                map.put(str, map.get(str) + 1);
+            }
+        }
+        return map;
     }
-
 }
